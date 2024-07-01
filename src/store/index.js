119214490f52e6bafbc9317cf35e8663db8becf2
harriselvin <harriselvin6@gmail.com>
2024-07-01 15:17:51 +0200

    fetched and added json data to index script

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,50 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
-  },
-  getters: {
+    aboutMe: null,
+    projects: null,
+    education: null,
+    skills: null,
+    workExp: null,
+    testimonials: null,
   },
   mutations: {
+    setAboutMe(state, payload) {
+      state.aboutMe = payload
+    },
+    setProjects(state, payload) {
+      state.projects = payload
+    },
+    setEducation(state, payload) {
+      state.education = payload
+    },
+    setSkills(state, payload) {
+      state.skills = payload
+    },
+    setWorkExp(state, payload) {
+      state.workExp = payload
+    },
+    setTestimonials(state, payload) {
+      state.testimonials = payload
+    }
   },
   actions: {
+    async getItem({commit}) {
+      let data = await axios.get('https://harriselvin.github.io/first_api/data/data.json')
+      let {aboutMe, projects, education, skills, workExp, testimonials} = data
+
+      commit('setAboutMe', aboutMe)
+      commit('setProjects', projects)
+      commit('setEducation', education)
+      commit('setSkills', skills)
+      commit('setWorkExp', workExp)
+      commit('setTestimonials', testimonials)
+    }
   },
   modules: {
+  },
+  getters: {
   }
 })
